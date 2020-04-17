52d3ac982a217cba38d8e1d27df50ab625d871d5
chanduarepalligit <63819355+chanduarepalligit@users.noreply.github.com>
2020-04-17 11:47:56 +0530

    Status code validation added

--- Api Test.py
+++ Api Test.py
@@ -4,5 +4,10 @@ import json
 
 
 resp = requests.get("https://reqres.in/api/users?page=2")
-#pprint(resp.json())
-print(resp.status_code)
+pprint(resp.json())
+resp_code = resp.status_code
+print(resp_code)
+if resp_code == 200:
+    print("Response fetched")
+else:
+    print("Failed to fetch response")
